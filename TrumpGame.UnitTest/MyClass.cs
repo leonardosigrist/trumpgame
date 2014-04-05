381cf8b4048fbb1fea81b3b7c1b698c915a692f7
Thiago <tsigwt@gmail.com>
2014-04-05 20:01:14 -0300

    add

---
+++ TrumpGame.UnitTest/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: thiago
+ * Date: 05/04/2014
+ * Time: 20:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace TrumpGame.UnitTest
+{
+    /// <summary>
+    /// Description of MyClass.
+    /// </summary>
+    public class MyClass
+    {
+        
+    }
+}
